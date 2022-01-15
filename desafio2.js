3ae8219e91704368a5d9662186ff45284ef3aa61
Juan Pablo Maison <maisonjuan911@gmail.com>
2022-01-15 09:15:19 -0300

    desafiojs

--- desafio2.js
+++ desafio2.js
@@ -1,6 +1,6 @@
 // compra de juegos y calculo de descuentos con iva incluido
 let entrada = prompt(
-  "Ingrese los juegos a comprar. Los juegos disponibles son God of War y Alone"
+    "Ingrese los juegos a comprar. Los juegos disponibles son God of War y Alone"
 );
 
 let juego1 = "God of War";
@@ -16,46 +16,37 @@ let precioTotal2 = (resultado2 * descuento) / 100;
 let precioConDto1 = resultado1 - precioTotal1;
 let precioConDto2 = resultado2 - precioTotal2;
 
-function calculo(
-  juego1,
-  juego2,
-  precioJuego1,
-  precioJuego2,
-  resultado1,
-  resultado2,
-  precioConDto1,
-  precioConDto2
-) {
+function calculo(juego1, juego2, precioJuego1, precioJuego2, resultado1, resultado2, precioConDto1, precioConDto2) {
 
-  if (entrada == juego1) {
-    alert("Usted eligio el juego " + juego1);
-    alert("El precio sin IVA de este juego es $" + precioJuego1);
-    alert("El precio de este juego con IVA es $" + resultado1);
-    alert(
-      "Este juego tiene un descuento del 30% y su precio final es $" +
-      precioConDto1
-    );
-  } else if (entrada == juego2) {
-    alert("Usted eligio el juego " + juego2);
-    alert("El precio sin IVA de este juego es $" + precioJuego2);
-    alert("El precio de este juego con IVA es $" + resultado2);
-    alert(
-      "Este juego tiene un descuento del 30% y su precio final es $" +
-      precioConDto2
-    );
-  } else {
-    alert("Este juego no existe");
-  }
+    if (entrada == juego1) {
+        alert("Usted eligio el juego " + juego1);
+        alert("El precio sin IVA de este juego es $" + precioJuego1);
+        alert("El precio de este juego con IVA es $" + resultado1);
+        alert(
+            "Este juego tiene un descuento del 30% y su precio final es $" +
+            precioConDto1
+        );
+    } else if (entrada == juego2) {
+        alert("Usted eligio el juego " + juego2);
+        alert("El precio sin IVA de este juego es $" + precioJuego2);
+        alert("El precio de este juego con IVA es $" + resultado2);
+        alert(
+            "Este juego tiene un descuento del 30% y su precio final es $" +
+            precioConDto2
+        );
+    } else {
+        alert("Este juego no existe");
+    }
 }
 
 
 calculo(
-  juego1,
-  juego2,
-  precioJuego1,
-  precioJuego2,
-  resultado1,
-  resultado2,
-  precioConDto1,
-  precioConDto2
+    juego1,
+    juego2,
+    precioJuego1,
+    precioJuego2,
+    resultado1,
+    resultado2,
+    precioConDto1,
+    precioConDto2
 );
