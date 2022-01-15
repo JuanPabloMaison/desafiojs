cd2b6abb08112e298a109658b795946afb244e24
Juan Pablo Maison <maisonjuan911@gmail.com>
2022-01-14 21:54:45 -0300

    desafiojs

--- desafio1.js
+++ desafio1.js
@@ -1,11 +1,11 @@
 // Pedir un número por prompt, repetir la salida del mensaje “Hola” la cantidad de veces ingresada.
 
-let numero = Number(prompt("Ingrese la cantidad de veces"));
-let saludo = "Hola";
+// let numero = Number(prompt("Ingrese la cantidad de veces"));
+// let saludo = "Hola";
 
-for (let i = 1; i <= numero; i++) {
-  document.write(saludo + "<br>");
-}
+// for (let i = 1; i <= numero; i++) {
+//   document.write(saludo + "<br>");
+// }
 
 // Pedir número mediante prompt y sumarle otro número en cada repetición,realizando una salida por cada resultado
 
